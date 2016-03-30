05123cc01f6d911af568056629f761343254870b
Giovani Gonzalez <darkgeat@gmail.com>
2016-03-30 11:24:58 -0600

    Whole screens done

--- app/src/main/java/com/giovani/fragments/Librero.java
+++ app/src/main/java/com/giovani/fragments/Librero.java
@@ -8,27 +8,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.giovani.greenhat.*;
+import com.giovani.greenhat.Paginas;
 
 /**
  * Created by DarkGeat on 3/28/2016.
  */
-public class Librero extends Fragment {
+public class Librero extends Fragment implements View.OnClickListener{
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_librero,container,false);
 
-        Button book = (Button)v.findViewById(R.id.button);
-        book.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), com.giovani.greenhat.Paginas.class);
-                startActivity(intent);
-            }
-        });
-
+        ImageView book = (ImageView) v.findViewById(R.id.book1);
+        book.setOnClickListener(this);
         return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        if (v.getId() == R.id.book1){
+            intent = new Intent(getActivity(), com.giovani.greenhat.Paginas.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/giovani/fragments/Menu.java
+++ app/src/main/java/com/giovani/fragments/Menu.java
@@ -1,24 +1,42 @@
 package com.giovani.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.giovani.greenhat.R;
 
 /**
  * Created by DarkGeat on 3/29/2016.
  */
-public class Menu extends Fragment {
+public class Menu extends Fragment implements View.OnClickListener{
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_menu,container,false);
 
+        ImageView catologo = (ImageView)v.findViewById(R.id.libreriaButton);
+        catologo.setOnClickListener(this);
+
+        ImageView catalogoIcon = (ImageView)v.findViewById(R.id.buttonCatalogo);
+        catalogoIcon.setOnClickListener(this);
+
         return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        if (v.getId() == R.id.libreriaButton || v.getId() == R.id.buttonCatalogo){
+            intent = new Intent(getActivity(), com.giovani.greenhat.Librero.class);
+            startActivity(intent);
+        }
     }
 }
